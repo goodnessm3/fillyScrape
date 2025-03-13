4874d5a0c0a6a3a2d631744d55859e8bf075de6a
goodnessm3 <cthulhu3125@gmail.com>
2025-03-12 21:17:28 -0400

    site generation

--- fillySoundDownloader.py
+++ fillySoundDownloader.py
@@ -18,7 +18,7 @@ from urllib.parse import unquote
 import json
 from fillyLog import my_log
 from urllib.parse import urlparse
-import subprocess as sp
+import subprocess
 import sqlite3
 import shutil
 
@@ -28,6 +28,7 @@ cursor.execute("CREATE TABLE IF NOT EXISTS files (hash TEXT PRIMARY KEY, fnumber
 
 sound_file_getter = re.compile(r'''sound=([^\]]*)''')
 sound_name_getter = re.compile(r'''[^\[]*''')
+
 
 def least_recent_op_number():
 
@@ -44,16 +45,6 @@ def least_recent_op_number():
                 lowest = postno
 
     return lowest
-
-
-def make_image_url(post):
-
-    # fname = post.get("filename", None)
-    fnumber = post.get("tim", None)
-    ex = post.get("ext", None)
-
-    if fname and fnumber and ex:
-        return CONTENT_ROOT.format(filename=fnumber, extension=ex)
 
 
 def is_soundpost(post):
@@ -89,45 +80,75 @@ def extract_soundpost(post):
         sound_link = "https://" + sound_link  # absolutely need this to avoid the redirect from catbox
 
     return sound_link, CONTENT_ROOT.format(filename=fnumber, extension=ex), stripped_fname, fnumber, ex, hash
-    
+
+
+def make_ffmpeg_command(vdest, adest, mdest):
+
+    """We need a slightly different command depending on whether the sound is accompanied by a video or a still image"""
+
+    _, video_ext = os.path.splitext(vdest)
+    video_ext = video_ext.lower()  # .webm, .mp4, or a still image
+
+    if video_ext == ".webm" or video_ext == ".mp4":  # the most common situation
+        vcmd = f'''ffmpeg -v error -i {vdest} -i {adest} -c:v copy -c:a libopus -b:a 128k {mdest}'''
+    else:  # we got a still image
+        mroot, _ = os.path.splitext(mdest)  # we've been passed an image file but we want to write a .webm
+        mdest = mroot + ".webm"
+        vcmd = f'''ffmpeg -v error -loop 1 -i {vdest} -i {adest} -c:v libvpx-vp9 -b:v 1M -c:a libopus -b:a 128k -shortest -vf format=yuv420p {mdest}'''
+
+    return vcmd, mdest  # in case mdest was changed due to a picture not a video
+
 
 def dl_soundpost(tup):
-
     """Expects a tuple from the extract_soundpost function"""
 
     snd, video, fname, fnumber, ext, hash = tup
     fnumbers = str(fnumber)
-    
+
     parsed_url = urlparse(snd)  # we are getting the file name to save to
     snd_name = os.path.basename(parsed_url.path)
 
     if file_seen(hash, snd_name):
-        return False  # we didn't download a video, so whatever loop is calling this can try again with the next post
-    
+        return False
+
     vdest = os.path.join(video_dest, fnumbers + ext)
     adest = os.path.join(audio_dest, snd_name)
     mdest = os.path.join(muxed_dest, fnumbers + ext)
     thdest = os.path.join(thumbs_dest, fnumbers + ".jpg")
 
-    result_codes = ""  # accumulate the return codes of the processes so we can see if they went wrong
-    
-    st1 = f'''curl {snd} > {adest}'''
-    res = os.system(st1)
-    result_codes += str(res)
-    st2 = f'''curl {video} > {vdest}'''
-    res = os.system(st2)
-    result_codes += str(res)
-    vcmd = f'''ffmpeg -i {vdest} -i {adest} -c:v copy -c:a libopus -b:a 128k {mdest}'''
-    res = os.system(vcmd)
-    result_codes += str(res)
-    thumb_cmd = f'''ffmpeg -i {mdest} -vf "select=eq(n\,0)" -frames:v 1 -q:v 2 \'{thdest}\''''
-    res = os.system(thumb_cmd)
-    result_codes += str(res)
+    result_codes = ""
+
+    my_log(f"starting to download soundpost: {hash},{snd_name},{fnumber}")
+
+    with open("curl.log", "a") as log:
+        # cm1 = f'''curl {snd} > {adest}'''
+        # cm2 = f'''curl {video} > {vdest}'''
+
+        with open(adest, "w") as af:
+            res = subprocess.run(f'''curl -sS {snd}'''.split(" "), stdout=af, stderr=log, timeout=30)
+            # curl -sS: silent and show errors
+            result_codes += str(res.returncode)
+        with open(vdest, "w") as vf:
+            res = subprocess.run(f'''curl -sS {video}'''.split(" "), stdout=vf, stderr=log, timeout=30)
+            result_codes += str(res.returncode)
+
+    with open("ffmpeg.log", "a") as log:
+        vcmd, updated_dest = make_ffmpeg_command(vdest, adest, mdest)
+        try:
+            res = subprocess.run(vcmd.split(" "), stdout=log, stderr=log, timeout=30)
+            result_codes += str(res.returncode)
+        except subprocess.TimeoutExpired:
+            my_log("ffmpeg was killed after taking too long to mux the files together.")
+            result_codes += str(-1)
+
+        thumb_cmd = f'''ffmpeg -v error -i {updated_dest} -vf select=eq(n\,0) -frames:v 1 -q:v 2 {thdest}'''
+        res = subprocess.run(thumb_cmd.split(" "), stdout=log, stderr=log)
+        result_codes += str(res.returncode)
 
     save_file(hash, snd_name, snd, fnumber, fname, result_codes)
     my_log(f"archived a soundpost: {hash},{snd_name},{fnumber}")
 
-    return True  # we downloaded a video so the loop should break, to not scrape too much at once
+    return True
     
     
 def file_seen(file_hash, soundurl):
@@ -140,7 +161,8 @@ def file_seen(file_hash, soundurl):
 
 def save_file(file_hash, snd, soundurl, fnumber, orifilename, success):
 
-    """Make a new entry after processing a soundpost, regardless of whether it was successful (we can check failed ones later)"""
+    """Make a new entry after processing a soundpost, regardless of whether it was successful (we can check failed ones
+    later)"""
     
     cursor.execute("INSERT INTO files (hash, soundurl, fnumber, orifilename, success) VALUES (?, ?, ?, ?, ?)", (file_hash + "|" +  snd, soundurl, fnumber, orifilename, success))
     conn.commit()
