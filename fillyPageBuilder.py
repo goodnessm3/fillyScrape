c110148a92d411b8dbc279c9be427424e1610ca4
goodnessm3 <>
2025-05-27 08:46:57 -0400

    fix extension handling

--- fillyPageBuilder.py
+++ fillyPageBuilder.py
@@ -69,6 +69,8 @@ for filename in thumblist:
         oriname, oriext, date = get_soundpost_data(fnum)
         if oriext.lower() in IMAGE_EXTENSIONS:
             newext = ".webm"  # image extension is irrelevant, we made it into a webm
+        else:
+            newext = oriext  # no change needed
         video_name = fnum + newext  # 05/26 10:57!!
         video_path = muxed_folder + "/" + video_name
         thumbnail_path = thumbnail_folder + "/" + filename
